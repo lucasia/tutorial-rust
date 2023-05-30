546ccc97695b0807775c561e3298b76a39fe25e3
Ian Lucas <ian.lucas@gmail.com>
2023-05-30 18:18:59 +0200

    Remove adder/target and add .gitignore

--- adder/target/debug/deps/adder-2be33b9b324550dd.d
+++
@@ -1,7 +0,0 @@
-/mnt/c/Users/ianlu/proj/tutorials/tutorial-rust/adder/target/debug/deps/adder-2be33b9b324550dd: src/lib.rs src/rectangle.rs src/guess.rs
-
-/mnt/c/Users/ianlu/proj/tutorials/tutorial-rust/adder/target/debug/deps/adder-2be33b9b324550dd.d: src/lib.rs src/rectangle.rs src/guess.rs
-
-src/lib.rs:
-src/rectangle.rs:
-src/guess.rs:
