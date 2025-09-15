67289c7375e1b026d2becfa1353beebd5735d370
Ian Lucas <ian.lucas@gmail.com>
2025-09-15 08:22:54 -0400

    feat: Add Ch 4. Ownership

---
+++ src/ch4_ownership/main.rs
@@ -0,0 +1,8 @@
+mod ownership;
+
+fn main() {
+
+    // TODO - 4.2 ref and borrowing and 4.3 the slice type
+    ownership::ownership();
+}
+
---
+++ src/ch4_ownership/ownership.rs
@@ -0,0 +1,79 @@
+pub fn ownership() {
+    move_example_one();
+    move_example_two();
+    clone_example();
+
+    function_ownership_example();
+    return_values_example();
+}
+
+
+fn move_example_one() {
+    let x = 5;
+    let y = x;
+    println!("x: {}, y: {}", x, y);
+
+
+    let s1 = String::from("hello");
+    let s2 = s1;    // s1 moved, no longer able to be used!
+
+    println!("{s2}, world!");
+}
+
+fn move_example_two() {
+    let mut s = String::from("hello");
+    println!("{s}");
+    s = String::from("ahoy");
+
+    println!("M2: {s}, world!");
+}
+
+fn clone_example() {
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1={s1}, s2={s2}");
+}
+
+// ======================
+// function ownership
+// ======================
+fn function_ownership_example() {
+    let s = String::from("hello");
+    takes_ownership(s); // s moved to function
+    // s no longer available to us!
+    let x = 5;
+    makes_copy(x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{some_string}");
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{some_integer}");
+}
+
+// ======================
+// return values and scope
+// ======================
+
+fn return_values_example() {
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);  // s2 is moved here
+
+    // s2 no longer available to us
+    
+    println!("s1={s1}, s3={s3}");
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into scope
+    a_string // a_string is returned moves out to the calling function
+}
