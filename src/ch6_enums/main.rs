00417e8ddd20eadd8ee54323d25bc5818cf1c33a
Ian Lucas <ian.lucas@gmail.com>
2025-09-21 11:24:02 -0400

    fix: Fix Chater number typo

--- src/ch6_enums/main.rs
+++ src/ch6_enums/main.rs
@@ -7,12 +7,12 @@ use log::info;
 fn main() {
     env_logger::init();
 
-    info!("========== 5.3 if let ==========");
+    info!("========== 6.3 if let ==========");
     if_let::if_let();
 
-    info!("========== 5.2 match flow ==========");
+    info!("========== 6.2 match flow ==========");
     match_flow::match_flow();
 
-    info!("========== 5.1 define enums ==========");
+    info!("========== 6.1 define enums ==========");
     enum_def::enum_def();
 }
