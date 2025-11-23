2148abaf1cdb379574cfb94ae025c5a7eadd5732
Ian Lucas <ian.lucas@gmail.com>
2025-11-23 17:22:37 -0500

    feat: Add Ch15 derefs (dereferences)

---
+++ crates/ch15_smart_ptr/src/derefs.rs
@@ -0,0 +1,61 @@
+use std::ops::Deref;
+use log::debug;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub fn derefs() {
+    debug!("====== in deref examples ======");
+
+    deref_int();
+    deref_box();
+    deref_mybox();
+    deref_coersion();
+}
+
+fn deref_coersion() {
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);  // deref coersion of `hello(&(*m)[..]);`
+}
+
+fn deref_mybox() {
+    let x = 5;
+    let y =  MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+fn hello(name: &str) {
+    debug!("Hello, {name}!");
+}
+
+
+fn deref_int() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+fn deref_box() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
--- crates/ch15_smart_ptr/src/main.rs
+++ crates/ch15_smart_ptr/src/main.rs
@@ -1,10 +1,14 @@
 use log::debug;
 
 mod boxes;
+mod derefs;
 
 fn main() {
     env_logger::init();
 
     debug!("====== boxes ======");
     boxes::boxes();
+
+    debug!("====== derefs ======");
+    derefs::derefs();
 }
